9d7799ceb1c1fe843653b828fc756c7eae6ae947
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-26 23:26:46 +0100

    Add files via upload

---
+++ hibernatespringboot/src/main/java/com/hibernatespringboot/hibernatespringboot/HibernatespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.hibernatespringboot.hibernatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatespringbootApplication.class, args);
+	}
+
+}
---
+++ hibernatespringboot/src/test/java/com/hibernatespringboot/hibernatespringboot/HibernatespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernatespringboot.hibernatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
